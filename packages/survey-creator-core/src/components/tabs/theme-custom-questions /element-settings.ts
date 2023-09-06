21fc73f5b8c9cc5e3d70cac2720b9676085dba50
bbyji07 <68051582+bbyji07@users.noreply.github.com>
2023-09-06 06:27:35 +0400

    Create element-settings.ts

---
+++ packages/survey-creator-core/src/components/tabs/theme-custom-questions /element-settings.ts
@@ -0,0 +1,73 @@
+import { ComponentCollection, IQuestion, QuestionCompositeModel } from "survey-core";
+import { getLocString } from "../../../editorLocalization";
+
+ComponentCollection.Instance.add({
+  name: "elementsettings",
+  showInToolbox: false,
+  elementsJSON: [
+    {
+      type: "colorsettings",
+      name: "backcolor",
+      colorTitle: getLocString("theme.backcolor"),
+      colorTitleLocation: "left",
+      titleLocation: "hidden",
+      descriptionLocation: "hidden"
+    },
+    {
+      type: "colorsettings",
+      name: "hovercolor",
+      colorTitle: getLocString("theme.hovercolor"),
+      colorTitleLocation: "left",
+      titleLocation: "hidden",
+      descriptionLocation: "hidden"
+    },
+    {
+      type: "spinedit",
+      name: "corner",
+      title: getLocString("theme.cornerRadius"),
+      titleLocation: "left",
+      descriptionLocation: "hidden",
+      unit: "px",
+      min: 0
+    },
+    {
+      type: "expression",
+      name: "cornerRadius",
+      expression: "iif({composite.corner} notempty, {composite.corner} + 'px', '')",
+      visible: false
+    }
+  ],
+  onInit() {
+  },
+  onCreated(question) {
+  },
+  onValueChanged(question, name, newValue) {
+  },
+});
+
+export function elementSettingsToCssVariable(question: IQuestion, themeCssVariables: {[index: string]: string}) {
+  Object.keys(question.value).forEach(key => {
+    if (key === "corner") return;
+    themeCssVariables[`--sjs-${question.name.toLocaleLowerCase()}-${key}`] = question.value[key];
+  });
+}
+
+export function elementSettingsFromCssVariable(question: IQuestion, themeCssVariables: {[index: string]: string}, defaultBackcolorVariable: string, defaultHovercolorVariable: string): any {
+  const result = {};
+  Object.keys(themeCssVariables).filter(key => key.indexOf(question.name.toLocaleLowerCase()) !== -1).forEach(key => {
+    const propertyName = key.split("-").pop();
+    if(propertyName === "cornerRadius") {
+      result["corner"] = parseFloat(themeCssVariables[key].toString());
+    } else {
+      result[propertyName] = themeCssVariables[key];
+    }
+  });
+
+  if(Object.keys(result).length === 0) {
+    (<QuestionCompositeModel>question).contentPanel.getQuestionByName("backcolor").value = defaultBackcolorVariable;
+    (<QuestionCompositeModel>question).contentPanel.getQuestionByName("hovercolor").value = defaultHovercolorVariable;
+  } else {
+    question.value = result;
+  }
+  return result;
+}
